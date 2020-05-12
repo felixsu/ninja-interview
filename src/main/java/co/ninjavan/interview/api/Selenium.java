f3265d8703cb1ffb9e20a240af3b53120356107f
Lanang Jati <>
2020-05-12 15:52:39 +0700

    add more method for selenium

--- src/main/java/co/ninjavan/interview/api/Selenium.java
+++ src/main/java/co/ninjavan/interview/api/Selenium.java
@@ -4,7 +4,58 @@ import org.openqa.selenium.WebElement;
 
 public interface Selenium {
 
-    void goToTab(String tabName);
+    /**
+     * this method is to change active tab
+     * @param indexTab Tab index, start with 0
+     */
+    void goToTab(int indexTab);
 
+    /**
+     * this method is to click given WebElement
+     * @param el WebElement
+     */
     void click(WebElement el);
+
+    /**
+     * this method is to double click given WebElement
+     * @param el WebElement
+     */
+    void doubleClick(WebElement el);
+
+    /**
+     * type string into the element
+     * @param el WebElement
+     * @param stringValue string to be typed into the input element
+     */
+    void fill(WebElement el, String stringValue);
+
+    /**
+     * clear any string on field element
+     * @param el WebElement
+     */
+    void clearField(WebElement el);
+
+    /**
+     * finish the block until element's visibility is true
+     * @param el WebElement
+     */
+    void waitUntilVisibilityOfElementLocated(WebElement el);
+
+    /**
+     * finish the block until element's visibility is false
+     * @param el WebElement
+     */
+    void waitUntilInvisibilityOfElementLocated(WebElement el);
+
+    /**
+     * scroll down until the end of page
+     */
+    void scrollDown();
+
+    /**
+     * check if the given element exist on page
+     * @param el WebElement that'll be checked
+     * @return true if element present on page. false if element is not present
+     */
+    boolean isElementExist(WebElement el);
 }
--- src/main/java/co/ninjavan/interview/api/SeleniumImpl.java
+++ src/main/java/co/ninjavan/interview/api/SeleniumImpl.java
@@ -12,7 +12,7 @@ public class SeleniumImpl implements Selenium {
     }
 
     @Override
-    public void goToTab(String tabName) {
+    public void goToTab(int indexTab) {
         throw new UnsupportedOperationException();
     }
 
@@ -21,4 +21,38 @@ public class SeleniumImpl implements Selenium {
         throw new UnsupportedOperationException();
     }
 
+    @Override
+    public void doubleClick(WebElement el) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void fill(WebElement el, String stringValue) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clearField(WebElement el) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void waitUntilVisibilityOfElementLocated(WebElement el) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void waitUntilInvisibilityOfElementLocated(WebElement el) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void scrollDown() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isElementExist(WebElement el) {
+        return false;
+    }
 }
